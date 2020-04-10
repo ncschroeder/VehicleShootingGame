a4034c800c92185d0fea3ec5415aac7dc7a27ed9
Nicholas Schroeder <nschroeder131@outlook.com>
2020-04-09 22:28:53 -0500

    Added buttons and score and time left text

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -7,16 +7,28 @@ public class Controls : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector3 xMovement;
-    float cameraRightXCoord, cameraLeftXCoord;
+    float maxRightXCoord, maxLeftXCoord;
     // Button moveLeftButton;
     void Start() 
     {
-        xMovement = new Vector3(0.3f, 0, 0);
-        cameraRightXCoord = Camera.main.orthographicSize * Screen.width / Screen.height;
-        cameraLeftXCoord = cameraRightXCoord * -1;
+        xMovement = new Vector3(2, 0, 0);
+        maxRightXCoord = Camera.main.orthographicSize * Screen.width / Screen.height - 2;
+        maxLeftXCoord = maxRightXCoord * -1;
         // moveLeftButton = this.get
     }
-    void Update()
+
+    public void moveLeft()
+	{
+        if (transform.position.x > maxLeftXCoord)
+            transform.position -= xMovement;
+    }
+
+    public void moveRight()
+	{
+        if (transform.position.x < maxRightXCoord)
+            transform.position += xMovement;
+    }
+    /*void Update()
     {   
         if (Input.GetKey("left"))
         {
@@ -28,5 +40,5 @@ public class Controls : MonoBehaviour
             if (transform.position.x < cameraRightXCoord)
                 transform.position += xMovement;
         }
-    }
+    }*/
 }
